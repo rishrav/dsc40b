d3289e56f70ed2e209afda59ca062548a338aae0
rishrav <71297201+rishrav@users.noreply.github.com>
2025-11-19 23:24:15 -0800

    Delete knn_distance.py

--- knn_distance.py
+++
@@ -1,7 +0,0 @@
-def knn_distance(arr, q, k):
-    distances = [(abs(x - q), x) for x in arr]
-    
-    distances.sort(key=lambda x: x[0])
-    
-    kth_distance, kth_point = distances[k - 1]
-    return (kth_distance, kth_point)
